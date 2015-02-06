9fbbdf9888e54dc7cbfd39d68909d61b3ab9327c
Mikael Zackrisson <micke.zackrisson@gmail.com>
2015-02-06 11:54:19 +0100

    using analytical noise gradient instead of finite difference to get noise normal

--- shaders/fs-terrain.glsl
+++ shaders/fs-terrain.glsl
@@ -95,21 +95,19 @@ vec4 permute(vec4 x) {
      return mod289(((x*34.0)+1.0)*x);
 }
 
-vec4 taylorInvSqrt(vec4 r)
-{
+vec4 taylorInvSqrt(vec4 r) {
   return 1.79284291400159 - 0.85373472095314 * r;
 }
 
-float snoise(vec3 v)
-  { 
+float snoise(vec3 v, out vec3 gradient) {
   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
   const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
 
-// First corner
+  // First corner
   vec3 i  = floor(v + dot(v, C.yyy) );
   vec3 x0 =   v - i + dot(i, C.xxx) ;
 
-// Other corners
+  // Other corners
   vec3 g = step(x0.yzx, x0.xyz);
   vec3 l = 1.0 - g;
   vec3 i1 = min( g.xyz, l.zxy );
@@ -123,15 +121,15 @@ float snoise(vec3 v)
   vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
 
-// Permutations
+  // Permutations
   i = mod289(i); 
   vec4 p = permute( permute( permute( 
              i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
            + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
            + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
 
-// Gradients: 7x7 points over a square, mapped onto an octahedron.
-// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+  // Gradients: 7x7 points over a square, mapped onto an octahedron.
+  // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
   float n_ = 0.142857142857; // 1.0/7.0
   vec3  ns = n_ * D.wyz - D.xzx;
 
@@ -161,18 +159,26 @@ float snoise(vec3 v)
   vec3 p2 = vec3(a1.xy,h.z);
   vec3 p3 = vec3(a1.zw,h.w);
 
-//Normalise gradients
+  //Normalise gradients
   vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
   p3 *= norm.w;
 
-// Mix final noise value
+  // Mix final noise value
   vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-  m = m * m;
-  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
-                                dot(p2,x2), dot(p3,x3) ) );
+  vec4 m2 = m * m;
+  vec4 m4 = m2 * m2;
+  vec4 pdotx = vec4(dot(p0,x0), dot(p1,x1), dot(p2,x2), dot(p3,x3));
+
+  // Determine noise gradient
+  vec4 temp = m2 * m * pdotx;
+  gradient = -8.0 * (temp.x * x0 + temp.y * x1 + temp.z * x2 + temp.w * x3);
+  gradient += m4.x * p0 + m4.y * p1 + m4.z * p2 + m4.w * p3;
+  gradient *= 42.0;
+
+  return 42.0 * dot(m4, pdotx);
 }
 
 
@@ -196,14 +202,22 @@ float noise( vec3 x )
       mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
 }
 
-float rockHeight(vec3 p) {
-	float n = 0.0;
-  p.y *= 0.0;
-	n += (snoise(1.5*p)+1.0)*0.8;
-  n += (snoise(3.0*p)+1.0)*0.4;
-  n += (snoise(6.0*p)+1.0)*0.2;
-  n += (snoise(12.0*p)+1.0)*0.1;
-	return n;
+vec3 rockNormal(vec3 p) {
+  vec3 normal = vec3(0.0,1.0,0.0);
+  float amp = 0.08;
+  vec3 grad;
+  float temp;
+  //p.y *= 2.0;
+	temp = snoise(1.5*p, grad);
+  normal += grad*amp;
+  temp = snoise(3.0*p, grad);
+  normal += grad*0.8*amp;
+  temp = snoise(6.0*p, grad);
+  normal += grad*0.6*amp;
+  temp = snoise(12.0*p, grad);
+  normal += grad*0.4*amp;
+
+  return normalize(normal);
 }
 
 vec3 rockColor(vec3 p) {
@@ -211,8 +225,9 @@ vec3 rockColor(vec3 p) {
   vec3 rockColor2 = vec3(84.0/255.0, 76.0/255.0, 71.0/255.0);
 
   float n = 0.0;
+  vec3 grad;
   for(float i = 1.0; i < 3.0; i += 1.0) {
-    n += (1.0/pow(2.0,i*0.5))*(snoise(0.2*p*pow(2.0,i))+1.0);
+    n += (1.0/pow(2.0,i*0.5))*(snoise(0.2*p*pow(2.0,i), grad)+1.0);
   }
 
   vec3 color = mix(rockColor1, rockColor2, smoothstep(0.8, 1.5,n));
@@ -221,9 +236,7 @@ vec3 rockColor(vec3 p) {
   return color;
 }
 
-float waterHeight( vec2 p )
-{
-  float smallWaveGain = 0.8;
+float waterHeight(vec2 p) {
   float height = 0.0;
 
   vec2 shift1 = -0.001*vec2( uTime*160.0*2.0, uTime*120.0*2.0 );
@@ -235,10 +248,10 @@ float waterHeight( vec2 p )
   wave += sin(p.x*0.03+p.y*0.01+shift2.x*4.2)*2.5 ;
   wave *= 0.05;
 
-  wave += (snoise(vec2(p*0.004 + shift1))-.5)*smallWaveGain*0.5;
-  wave += (snoise(vec2(p*0.010 + shift1*1.3))-.5)*smallWaveGain*0.15;
+  wave += (snoise(vec2(p*0.004 + shift1))-.5)*0.8*0.5;
+  wave += (snoise(vec2(p*0.010 + shift1*1.3))-.5)*0.8*0.15;
 
-  float amp = smallWaveGain;
+  float amp = 0.8;
   float smoothamp = 1.0;
 
   for (float i=0.0; i<5.0; i+=1.0) {
@@ -260,8 +273,8 @@ float waterHeight( vec2 p )
 }
 
 vec3 lightPosition() {
-  //return normalize(vec3(0.0, 1.0, 0.5));
-  return normalize(vec3(0.0, sin(-uTime*uSpeed), cos(-uTime*uSpeed)));
+  return normalize(vec3(0.0, 1.0, 0.5));
+  //return normalize(vec3(0.0, sin(-uTime*uSpeed), cos(-uTime*uSpeed)));
 }
 
 vec3 lightColor(vec3 lightPos) {
@@ -299,12 +312,12 @@ void main() {
 	vec3 yDiff = vec3(0.0, 0.0, 1.0)*delta;
 
   vec3 finalRockColor = rockColor(vWorldSpaceCoord);
-  vec3 surfaceNormal;
+  vec3 surfaceNormal = vec3(0.0,1.0,0.0);
   float diff;
 
   // no need to do bump mapping where the water will be fully opaque
   if( vWorldSpaceCoord.y > -3.0) {
-  	surfaceNormal = normalize(vec3(rockHeight(vWorldSpaceCoord-xDiff) - rockHeight(vWorldSpaceCoord+xDiff), 1.0, rockHeight(vWorldSpaceCoord-yDiff) - rockHeight(vWorldSpaceCoord+yDiff)));
+    vec3 surfaceNormal = rockNormal(vWorldSpaceCoord);
 
     vec3 objectNormal = vWorldSpaceNormal;
     vec3 tangent = normalize(cross(objectNormal, vec3(0.0,1.0,0.0)));
